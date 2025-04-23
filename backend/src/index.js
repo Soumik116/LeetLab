4f1ed90aa2558eed2329a3036d49ff400f9eb65c
Soumik Das <soumikd112@gmail.com>
2025-04-23 09:48:03 +0530

    basic setup of backend like prisma and prisma client intialization . register , login , logout and check router added and basic boilerplate code

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+import express from "express"
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.routes.js"
+
+
+dotenv.config()
+
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req,res) => {
+    res.send("Hello guys welcome to LeetLab 🔥")
+})
+
+app.use("/api/v1/auth", authRoutes)
+
+
+app.listen(process.env.PORT || 4000 , () => {
+    console.log("Server is running on port 8080")
+})
