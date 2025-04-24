2c0476ff17ae8b190a80c341104a039e794a64b8
Soumik Das <soumikd112@gmail.com>
2025-04-24 23:59:22 +0530

    check controller added. authmiddleware added in logout and check routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { check, login, logout, register } from "../controllers/auth.controllers.js";
+import {
+  check,
+  login,
+  logout,
+  register,
+} from "../controllers/auth.controllers.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const authRoutes = express.Router();
 
@@ -7,8 +13,8 @@ authRoutes.post("/register", register);
 
 authRoutes.post("/login", login);
 
-authRoutes.post("/logout", logout);
+authRoutes.post("/logout", authMiddleware, logout);
 
-authRoutes.get("/check", check);
+authRoutes.get("/check", authMiddleware, check);
 
 export default authRoutes;
